568061e47f247d33d83d33013f5482b1996f44ad
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-08-07 05:26:16 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments) 
+    /* *arguments is an arr of argument, which is hash */
+    result = []
+    argument.each do |k, v|
+      # argument is kinda a hash
+      if argument == v 
+        result << k
+      end 
+    end 
+    result
   end
 end
