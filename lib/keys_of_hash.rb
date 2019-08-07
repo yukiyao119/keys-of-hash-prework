4d25ab8dd5ed7af8166c56d083b27520da961868
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-08-07 05:31:14 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -16,9 +16,10 @@ class Hash
 end
 
 
-{"sugar glider"=>"Australia",
-"aye-aye"=> "Madagascar",
-"red-footed tortoise"=>"Panama",
-"kangaroo"=> "Australia",
-"tomato frog"=>"Madagascar",
-"koala"=>"Australia"}
+animal = {
+  "sugar glider"=>"Australia",
+  "aye-aye"=> "Madagascar",
+  "red-footed tortoise"=>"Panama",
+  "kangaroo"=> "Australia",
+  "tomato frog"=>"Madagascar",
+  "koala"=>"Australia"}
