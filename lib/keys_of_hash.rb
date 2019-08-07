1b3d812144dd59b59e46944f43e1015de5e0379e
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-08-07 05:43:18 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,26 @@
+require 'pry'
+
+animal = {
+  "sugar glider"=>"Australia",
+  "aye-aye"=> "Madagascar",
+  "red-footed tortoise"=>"Panama",
+  "kangaroo"=> "Australia",
+  "tomato frog"=>"Madagascar",
+  "koala"=>"Australia"
+}
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments) # *arguments is an arr of argument, which is hash 
+    result = []
+    self.each do |k, v|
+      arguments.each do |element|
+        if v == element
+          result << k
+        end 
+      end  
+    end 
+    result
+    # binding.pry 
   end
 end
+
